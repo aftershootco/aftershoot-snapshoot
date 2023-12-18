bd5003a6206cfe54c8f0ee12a5f544d9a64d1132
booi-dev <booimangang@gmail.com>
2023-12-18 20:39:51 +0530

    create: new template with image links

---
+++ data.js
@@ -0,0 +1,12 @@
+const sitelogo = "https://download.aftershoot.com/email-images/Logo.png";
+const user = "https://download.aftershoot.com/email-images/User_Image.png";
+const gradientYext =
+  "https://download.aftershoot.com/email-images/aftershoot-gradiant.png";
+const crab = "https://download.aftershoot.com/email-images/crab.png";
+const wedding = "https://download.aftershoot.com/email-images/wedding.png";
+const wf = "https://download.aftershoot.com/email-images/Workflow_A_Lr.png";
+const cmaera = "https://download.aftershoot.com/email-images/Camera.png";
+const founder =
+  "https://download.aftershoot.com/email-images/Harshit_Hoodie.png";
+const social =
+  "https://download.aftershoot.com/email-images/iconsInstagram.png";
